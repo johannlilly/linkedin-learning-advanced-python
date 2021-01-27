e84bc11e330fcc46c3a3a66caf31e48297571463
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-27 15:08:42 -0600

    Transforms: list(), filter(), map()

--- exercise-files/02 BuiltIn Functions/transforms_start.py
+++ exercise-files/02 BuiltIn Functions/transforms_start.py
@@ -2,19 +2,32 @@
 
 
 def filterFunc(x):
-    pass
+    if x % 2 == 0:
+        return False
+    return True
 
 
 def filterFunc2(x):
-    pass
+    if x.isupper():
+        return False
+    return True
 
 
 def squareFunc(x):
-    pass
+    return x**2
 
 
 def toGrade(x):
-    pass
+    if 90 <= x <= 100:
+        return "A"
+    elif 80 <= x < 90:
+        return "B"
+    elif 70 <= x < 80:
+        return "C"
+    elif 65 <= x < 70:
+        return "D"
+    else:
+        return "F"
 
 
 def main():
@@ -24,12 +37,21 @@ def main():
     grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
     # TODO: use filter to remove items from a list
+    odds = list(filter(filterFunc, nums))
+    print(odds)
 
     # TODO: use filter on non-numeric sequence
+    lowers = list(filter(filterFunc2, chars))
+    print(lowers)
 
     # TODO: use map to create a new sequence of values
+    squares = list(map(squareFunc, nums))
+    print(squares)
 
     # TODO: use sorted and map to change numbers to grades
+    grades = sorted(grades)
+    letters = list(map(toGrade, grades))
+    print(letters)
 
 
 if __name__ == "__main__":
