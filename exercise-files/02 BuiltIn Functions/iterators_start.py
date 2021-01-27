b32b29ca28dc8c05f64ff7fc30b36ba0ec26675b
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-27 14:02:08 -0600

    Iterators - next(), zip(), enumerate()

--- exercise-files/02 BuiltIn Functions/iterators_start.py
+++ exercise-files/02 BuiltIn Functions/iterators_start.py
@@ -7,14 +7,34 @@ def main():
     daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
 
     # TODO: use iter to create an iterator over a collection
+    i = iter(days)
+    print(next(i))
+    print(next(i))
+    print(next(i))
+    print(next(i))
 
     # TODO: iterate using a function and a sentinel
+    with open("testfile.txt", "r") as fp:
+        for line in iter(fp.readline, ''):
+            print(line)
 
     # TODO: use regular interation over the days
+    for m in range(len(days)):
+        print(m+1, days[m])
 
     # TODO: using enumerate reduces code and provides a counter
+    for i,m in enumerate(days, start=1):
+        print(i, m)
 
     # TODO: use zip to combine sequences
+    # multiple sequences at the same time
+    for m in zip(days, daysFr):
+        print(m)
+    # combine with enumerate
+    for i,m in enumerate(zip(days, daysFr), start=1):
+        print(i, m[0], "=", m[1], "in French")
+    # if not the same length, the zip function terminates
+    # when the shortest sequence is exhausted
 
 
 if __name__ == "__main__":
