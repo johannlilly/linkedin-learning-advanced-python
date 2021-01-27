a2bc580399ea9e923e857a0df009ea12d24abdef
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-27 14:01:40 -0600

    Utilities - any, all, min, max, sum

--- exercise-files/02 BuiltIn Functions/utilfuncs_start.py
+++ exercise-files/02 BuiltIn Functions/utilfuncs_start.py
@@ -4,14 +4,25 @@
 def main():
     # use any() and all() to test sequences for boolean values
     list1 = [1, 2, 3, 0, 5, 6]
+    tuple1 = {1, 2, 3, 0, 5, 6}
     
     # TODO: any will return true if any of the sequence values are true
-    
+    print(any(list1))
+    print(any(tuple1))
+
     # TODO: all will return true only if all values are true
-    
+    print(all(list1))
+    print(all(tuple1))
+
     # TODO: min and max will return minimum and maximum values in a sequence
-    
+    print(min(list1))
+    print(max(list1))
+    print(min(tuple1))
+    print(max(tuple1))
+
     # TODO: Use sum() to sum up all of the values in a sequence
+    print(sum(list1))
+    print(sum(tuple1))
     
 if __name__ == "__main__":
     main()
