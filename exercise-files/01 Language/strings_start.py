513308c85fa3b4c5979d10f5d3fc6a4280074012
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-27 11:08:01 -0600

    Strings vs. bytes
    Strings and bytes are not the same thing

--- exercise-files/01 Language/strings_start.py
+++ exercise-files/01 Language/strings_start.py
@@ -10,11 +10,19 @@ def main():
     print(s)
     
     # TODO: Try combining them. 
+    print(s+b)
     
     # TODO: Bytes and strings need to be properly encoded and decoded
     # before you can work on them together
-    
+    s2 = b.decode('utf-8')
+    print(s+s2)
+
+    b2 = s.encode('utf-8')
+    print(b+b2)    
+
     # TODO: encode the string as UTF-32
+    b3 = s.encode('utf-32')
+    print(b3)
     
 if __name__ == "__main__":
     main()
