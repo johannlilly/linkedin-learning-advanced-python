333de329ffec2822f41c02257289d95a2e04ec1c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-27 16:03:14 -0600

    Function documentation strings

--- exercise-files/03 Functions/docstrings_start.py
+++ exercise-files/03 Functions/docstrings_start.py
@@ -2,6 +2,12 @@
 
 
 def myFunction(arg1, arg2=None):
+    """myFunction(arg1, arg2=None) --> Doesn't really do anything, just prints.
+
+    Parameters:
+    arg1: the first argumetn. Whatever you feel like passing.
+    arg2: second argiment. Defaults to None.
+    """
     print(arg1, arg2)
 
 
