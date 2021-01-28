a9edb8add4a108bf4fa6bf8fd22d5182715f725c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-28 10:26:13 -0600

    Lambda functions

--- exercise-files/03 Functions/lambdas_start.py
+++ exercise-files/03 Functions/lambdas_start.py
@@ -14,9 +14,12 @@ def main():
     ftemps = [32, 65, 100, 212]
 
     # TODO: Use regular functions to convert temps
+    print(list(map(FahrenheitToCelsisus, ftemps)))
+    print(list(map(CelsisusToFahrenheit, ctemps)))
 
     # TODO: Use lambdas to accomplish the same thing
-
+    print(list(map(lambda t: (temp-32) * 5/9, ftemps)))
+    print(list(map(lambda t: (temp * 9/5) + 32, ctemps)))
 
 if __name__ == "__main__":
     main()
