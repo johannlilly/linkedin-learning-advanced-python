396f9756c851ee65a2f87da8c68ca41d82b53107
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-28 15:28:21 -0600

    Collections: defaultdict()

--- exercise-files/04 Collections/defaultdict_start.py
+++ exercise-files/04 Collections/defaultdict_start.py
@@ -1,5 +1,6 @@
 # Demonstrate the usage of defaultdict objects
 
+from collections import defaultdict
 
 def main():
     # define a list of items that we want to count
@@ -7,7 +8,10 @@ def main():
               'apple', 'grape', 'banana', 'banana']
 
     # use a dictionary to count each element
-    fruitCounter = {}
+    # if I try to access a key that doesn't exist, 
+    # create a default value for me using
+    # `int` object as a constructor
+    fruitCounter = defaultdict(int)
 
     # Count the elements in the list
     for fruit in fruits:
