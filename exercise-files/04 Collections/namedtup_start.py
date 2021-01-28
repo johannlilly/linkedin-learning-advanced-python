969be438c55bdc7b74f804fca79dade2124fb7c4
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-28 15:02:18 -0600

    Collections: namedtuple()
    Makes code easier to read and scale

--- exercise-files/04 Collections/namedtup_start.py
+++ exercise-files/04 Collections/namedtup_start.py
@@ -5,9 +5,16 @@ import collections
 
 def main():
     # TODO: create a Point namedtuple
+    Point = collections.namedtuple("Point", "x y")
+    p1 = Point(10,20)
+    p2 = Point(30,40)
+    print(p1, p2)
+    # use descriptive names to access the data
+    print(p1.x, p2.y)
 
-    # TODO: use _replace to create a new instance
-    pass
+    # TODO: use _replace to create a new instance (a helper function)
+    p1 = p1._replace(x=100)
+    print(p1)
 
 
 if __name__ == "__main__":
