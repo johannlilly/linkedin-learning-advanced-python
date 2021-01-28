d9a487ab42c6afb69570d8e3884cd367b16535da
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-28 10:55:49 -0600

    Keyword-only arguments (separate with asterisk)

--- exercise-files/03 Functions/keyargs_start.py
+++ exercise-files/03 Functions/keyargs_start.py
@@ -1,14 +1,19 @@
 # Demonstrate the use of keyword-only arguments
-
+'''
+you can separate positional arguments from arguments
+which are keyword-only using an asterisk
+'''
 
 # use keyword-only arguments to help ensure code clarity
-def myFunction():
+def myFunction(arg1, arg2, *, suppressExceptions=False):
     pass
 
 
 def main():
     # try to call the function without the keyword
     myFunction(1, 2, True)
+    # use the keyword
+    myFunction(1, 2, suppressExceptions=True)
 
 
 if __name__ == "__main__":
