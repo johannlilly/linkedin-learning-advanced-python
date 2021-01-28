0e5f9fa9d6ea7532e2bcfa36aaeeaab03641cdc6
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-28 09:51:32 -0600

    Variable argument lists: star operator (spread operator)

--- exercise-files/03 Functions/varargs_start.py
+++ exercise-files/03 Functions/varargs_start.py
@@ -2,16 +2,22 @@
 
 
 # TODO: define a function that takes variable arguments
-def addition():
-    pass
+def addition(*args):
+    result = 0
+    for arg in args:
+        result += arg
+    return result
 
 
 def main():
     # TODO: pass different arguments
-    print(addition())
+    print(addition(5, 10, 15, 20))
+    print(addition(5, 10, 15, 20))
 
     # TODO: pass an existing list
-
+    myNums = [5,10,15,20]
+    # the star operator here acts like a spread operator
+    print(addition(*myNums))
 
 if __name__ == "__main__":
     main()
