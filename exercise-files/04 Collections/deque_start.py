d21c27941bb988dc2d8512d29dc3fa10dc315977
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-28 18:30:27 -0600

    Collections: deque objects
    For if you need to be able to operate on both ends of a list, and perform operations such as rotation

--- exercise-files/04 Collections/deque_start.py
+++ exercise-files/04 Collections/deque_start.py
@@ -1,4 +1,8 @@
 # deque objects are like double-ended queues
+"""
+For if you need to be able to operate on both ends of a list,
+and perform operations such as rotation
+"""
 
 import collections
 import string
@@ -7,15 +11,26 @@ import string
 def main():
     
     # TODO: initialize a deque with lowercase letters
+    d = collections.deque(string.ascii_lowercase)
 
     # TODO: deques support the len() function
+    print("Item count: ", str(len(d)))
 
     # TODO: deques can be iterated over
+    for elem in d:
+        print(elem.upper(), end=",")
 
     # TODO: manipulate items from either end
+    d.pop()
+    d.popleft()
+    d.append(2)
+    d.appendleft(1)
+    print(d)
 
     # TODO: rotate the deque
-
+    print(d)
+    d.rotate(10)
+    print(d)
 
 if __name__ == "__main__":
     main()
