619301ec07a5f441abb544cf6e73b10caf12e269
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-02 16:20:23 -0600

    Python Comprehensions: Set comprehensions

--- exercise-files/07 Comprehensions/dictcomp_start.py
+++ exercise-files/07 Comprehensions/dictcomp_start.py
@@ -6,11 +6,19 @@ def main():
     ctemps = [0, 12, 34, 100]
 
     # TODO: Use a comprehension to build a dictionary
+    tempDict = {t: (t*9/5) +32 for t in ctemps if t < 100}
+    print(tempDict)
+    print(tempDict[12])
 
     # TODO: Merge two dictionaries with a comprehension
     team1 = {"Jones": 24, "Jameson": 18, "Smith": 58, "Burns": 7}
     team2 = {"White": 12, "Macke": 88, "Perce": 4}
 
+    # The items () method returns a view object.
+    # The view object contains the key-value pairs of the dictionary,
+    # as tuples in a list
+    newTeam = {k:v for team in (team1, team2) for k,v in team.items()}
+    print(newTeam)
 
 if __name__ == "__main__":
     main()
