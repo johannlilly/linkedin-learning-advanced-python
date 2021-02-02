ce11195cdcf0a8b9b4cbd168a0e444eb3545396b
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-02 15:48:56 -0600

    Using Logging: Custom logging
    Note: integrate with Splunk using HTTP Event Collector

--- exercise-files/06 Logging/customlog_start.py
+++ exercise-files/06 Logging/customlog_start.py
@@ -2,17 +2,28 @@
 
 import logging
 
+extData = {
+    'user' : 'joe@example.com'
+}
+
 # TODO: add another function to log from
+def anotherFunction():
+    logging.debug("This is a debug-level message", extra=extData)
 
 def main():
     # set the output file and debug level, and
     # TODO: use a custom formatting specification
+    fmtstr = "User:%(user)s %(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+    datestr = "%m/%d/%Y %I:%M:%S %p"
     logging.basicConfig(filename="output.log",
-                        level=logging.DEBUG)
+                        level=logging.DEBUG,
+                        filemode="w",
+                        format=fmtstr,
+                        datefmt=datestr)
 
-    logging.info("This is an info-level log message")
-    logging.warning("This is a warning-level message")
-
+    logging.info("This is an info-level log message", extra=extData)
+    logging.warning("This is a warning-level message", extra=extData)
+    anotherFunction()
 
 if __name__ == "__main__":
     main()
