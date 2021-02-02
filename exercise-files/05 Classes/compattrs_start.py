5673a4eee3fa49fe8cd7da091d4eb0cf2d66d221
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-02-02 13:45:51 -0600

    Advanced Classes and Objects: Computed Attributes

--- exercise-files/05 Classes/compattrs_start.py
+++ exercise-files/05 Classes/compattrs_start.py
@@ -9,27 +9,46 @@ class myColor():
 
     # TODO: use getattr to dynamically return a value
     def __getattr__(self, attr):
-        pass
+        if attr == "rgbcolor":
+            return (self.red, self.green, self.blue)
+        # calcular CSS-style HEX code
+        elif attr == "hexcolor":
+            return "#{0:02x}{1:02x}{2:02x}".format(
+                self.red, self.green, self.blue
+            )
+        else:
+            raise AttributeError
 
     # TODO: use setattr to dynamically return a value
     def __setattr__(self, attr, val):
+        if attr == "rgbcolor":
+            self.red = val[0]
+            self.green = val[1]
+            self.blue = val[2]
+        # call super so that __getattr__ is not called instead
         super().__setattr__(attr, val)
 
     # TODO: use dir to list the available properties
     def __dir__(self):
-        pass
+        return("red", "green", "blue", "rgbcolor", "hexcolor")
 
 def main():
     # create an instance of myColor
     cls1 = myColor()
     # TODO: print the value of a computed attribute
+    print(cls1.rgbcolor)
+    print(cls1.hexcolor)
 
     # TODO: set the value of a computed attribute
+    cls1.rgbcolor = (125, 200, 86)
+    print(cls1.rgbcolor)
+    print(cls1.hexcolor)
 
-    # TODO: access a regular attribute
+    # TODO: access a regular attribute (declared values)
+    print(cls1.red)
 
-    # TODO: list the available attributes
-
+    # TODO: list the available attributes (attributes that my object supports)
+    print(dir(cls1))
 
 if __name__ == "__main__":
     main()
